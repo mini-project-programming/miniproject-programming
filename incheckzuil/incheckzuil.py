3959f646301e483acfaeef1d6054c16505570c9d
Merlijn <merlijn@familie-wissink.com>
2015-10-13 14:31:31 +0200

    Functie gemaakt zodat de boel overzichtelijker is

--- incheckzuil/incheckzuil.py
+++ incheckzuil/incheckzuil.py
@@ -3,20 +3,23 @@ __author__ = 'Merlijn'
 import sqlite3
 
 database_file = "../reis-database.db"
-
 ov_nummer = 0
 gebruiker_ID = 0
 
+
+def haal_gegevens(ovnummer):
+    with sqlite3.connect(database_file) as conn:
+        c = conn.cursor()
+
+        c.execute("SELECT gebruikerID FROM gebruikers WHERE ovnummer=%d" % int(ovnummer))
+
+        for row in c.fetchone():
+            gebruiker_ID = row
+            print(row)
+
+        c.execute("SELECT * FROM reisgegevens WHERE gebruikerID=%d" % gebruiker_ID)
+        print(c.fetchall())
+
+
 ov_nummer = input("Wat is uw OV nummer?")
-
-with sqlite3.connect(database_file) as conn:
-    c = conn.cursor()
-
-    c.execute("SELECT gebruikerID FROM gebruikers WHERE ovnummer=%d" % int(ov_nummer))
-
-    for row in c.fetchone():
-        gebruiker_ID = row
-        print(row)
-
-    c.execute("SELECT * FROM reisgegevens WHERE gebruikerID=%d" % gebruiker_ID)
-    print(c.fetchall())
+haal_gegevens(ov_nummer)
