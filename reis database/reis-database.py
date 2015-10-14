62e209c3b2d0dcb3d7dc8ccdd8db82f592b64969
Eddie <e-w.1998@hotmail.nl>
2015-10-14 14:32:19 +0200

    meerdere opties toegevoegd

--- reis database/reis-database.py
+++ reis database/reis-database.py
@@ -34,16 +34,56 @@ def genereer_ovnummerlijst():
 
         return ovnummerlijst
 
+def window2():
+    window = tk.Tk()
+    window.geometry('400x200')
+    window.title("Beginstation")
+    window.configure(background='yellow')
 
-def windows():
+    option1 = window
+    variable = StringVar(option1)
+    variable.set('Beginstation')
+    om1 = OptionMenu(option1, variable, *station_lijst)
+
+    B = tk.Button(window, text ="Volgende", bd = 5, width = 50, height = 4, bg = 'yellow', activebackground = 'yellow', command = window3, state = 'normal')
+    w = Label(window, text="Uw opdracht is voltooid!", font=('Arial', 20), bg= 'yellow')
+
+    w.pack()
+    om1.place(x = 100, y = 100)
+    om1.pack()
+    B.place(x = 130, y = 300)
+    B.pack()
+
+
+def window3():
+    window = tk.Tk()
+    window.geometry('400x200')
+    window.title("Beginstation")
+    window.configure(background='yellow')
+
+    option2 = window
+    variable = StringVar(option2)
+    variable.set("Eindstation")
+    om2 = OptionMenu(option2, variable, *station_lijst)
+
+    B = tk.Button(window, text ="Volgende", bd = 5, width = 50, height = 4, bg = 'yellow', activebackground = 'yellow', command = window4, state = 'normal')
+    w = Label(window, text="Uw opdracht is voltooid!", font=('Arial', 20), bg= 'yellow')
+
+    w.pack()
+    om2.place(x = 100, y = 100)
+    om2.pack()
+    B.place(x = 130, y = 300)
+    B.pack()
+
+def window4():
     window = tk.Tk()
     window.geometry('600x400')
     window.title('Voltooid')
     window.configure(background='yellow')
     w = Label(window, text="Uw opdracht is voltooid!", font=('Arial', 20), bg= 'yellow')
+
     w.pack()
     w.place(x=170, y=150)
-    print(E2.get())
 
 
 # genereer eenmalig de gegevens die in de database staan zodat er makkelijk meer gewerkt kan worden
@@ -56,23 +96,13 @@ window.title('Ns overzicht')
 window.configure(background='yellow')
 
 T = Label(window, text="Welkom bij NS reis-database", font=('Arial', 20), bg= 'yellow')
-B = tk.Button(window, text ="Continue", bd = 5, width = 50, height = 4, bg = 'yellow', activebackground = 'yellow', command = windows, state = 'disabled')
+B = tk.Button(window, text ="Volgende", bd = 5, width = 50, height = 4, bg = 'yellow', activebackground = 'yellow', command = window2, state = 'disabled')
 
 sv = tk.StringVar()
 sv.trace("w", lambda name, index, mode, sv=sv: callback(sv))
 
 E1 = tk.Label(window, text="OV-nummer", bg = 'yellow')
 E2 = tk.Entry(window, textvariable=sv)
-
-option1 = window
-variable = StringVar(option1)
-variable.set('Beginstation')
-om1 = OptionMenu(option1, variable, *station_lijst)
-
-option2 = window
-variable = StringVar(option2)
-variable.set("Eindstation")
-om2 = OptionMenu(option2, variable, *station_lijst)
 
 def callback(sv):
     try:
@@ -85,21 +115,14 @@ def callback(sv):
     except:
         print('Retry')
 
-
-
-
 T.pack()
 E1.pack()
 E2.pack()
 B.pack()
-om1.pack()
-om2.pack()
 T.place(x = 140)
 E1.place(x = 130, y = 200)
 E2.place(x = 210, y = 200)
 B.place(x = 130, y = 300)
-om1.place(x = 100, y = 100)
-om2.place(x = 100, y = 150)
 window.mainloop()
 
 
