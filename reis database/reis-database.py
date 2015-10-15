b82bf944da6ccbfd94a54e13e34c8306d2e1601a
Eddie <e-w.1998@hotmail.nl>
2015-10-15 13:34:54 +0200

    .

--- reis database/reis-database.py
+++ reis database/reis-database.py
@@ -1,151 +1,200 @@
 __author__ = 'Merlijn'
 
-import sqlite3,qrcode
+import qrcode
 import tkinter as tk
 from tkinter import *
+from database import *
 
-database_file = "../reis-database.db"
+TITLE_FONT = ("Helvetica", 18, "bold")
+beginstation = ""
+eindstation = ""
+ovnummer = 0
 
 
 def genereer_stationlijst():
-    with sqlite3.connect(database_file) as conn:
-        stationlijst = []
-        c = conn.cursor()
+    stationlijst = []
 
-        c.execute("SELECT naam FROM stations ORDER BY naam ASC")
 
-        for row in c.fetchall():
-            for row2 in row:
-                stationlijst.append(row2)
+    cursor = database.query("SELECT naam FROM stations ORDER BY naam ASC")
 
-        return stationlijst
+    for row in database.fetchAll(cursor):
+        for row2 in row:
+            stationlijst.append(row2)
+
+    return stationlijst
 
 
 def genereer_ovnummerlijst():
-    with sqlite3.connect(database_file) as conn:
-        ovnummerlijst = []
-        c = conn.cursor()
+    ovnummerlijst = []
 
-        c.execute("SELECT ovnummer FROM gebruikers")
+    cursor = database.query("SELECT ovnummer FROM gebruikers")
 
-        for row in c.fetchall():
-            for row2 in row:
-                ovnummerlijst.append(row2)
+    for row in database.fetchAll(cursor):
+        for row2 in row:
+            ovnummerlijst.append(row2)
 
-        return ovnummerlijst
+    return ovnummerlijst
 
-def window2():
-    window = tk.Tk()
-    window.geometry('400x200')
-    window.title("Beginstation")
-    window.configure(background='yellow')
-    option1 = window
 
-    def option_changed(a):
-        if a != 'Beginstation':
+class reisDatabase(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (loginFrame, beginstationFrame, eindstationFrame, voltooidFrame):
+            frame = F(container, self)
+            self.frames[F] = frame
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(loginFrame)
+
+    def show_frame(self, c):
+        '''Show a frame for the given class'''
+        frame = self.frames[c]
+        frame.tkraise()
+
+
+class loginFrame(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        ovnummer_lijst = genereer_ovnummerlijst()
+
+        T = Label(self, text="Welkom bij NS reis-database", font=('Arial', 20))
+        B = tk.Button(self, text ="Volgende", bd = 5, width = 50, height = 4, command = lambda: controller.show_frame(beginstationFrame), state = 'disabled')
+
+        sv = tk.StringVar()
+        sv.trace("w", lambda name, index, mode, sv=sv: callback(sv))
+
+        E1 = tk.Label(self, text="OV-nummer (8 cijfers)")
+        E2 = tk.Entry(self, textvariable=sv)
+
+        def callback(sv):
+            global ovnummer
+            try:
+                if int(E2.get()) in ovnummer_lijst:
+                    print('Correct')
+                    B.config(state = 'normal')
+                    ovnummer = int(E2.get())
+                else:
+                    B.config(state = 'disabled')
+            except:
+                print('Retry')
+
+        T.pack()
+        E1.pack()
+        E2.pack()
+        B.pack()
+        T.place(x = 140)
+        E1.place(x = 180, y = 200)
+        E2.place(x = 310, y = 200)
+        B.place(x = 130, y = 300)
+
+
+class beginstationFrame(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        station_lijst = genereer_stationlijst()
+
+        def option_changed(a):
+            global beginstation
+            print(variable)
+            beginstation = variable.get()
             B.config(state='normal')
-        else:
-            B.config(state='disabled')
 
-    variable1 = StringVar()
-    variable1.set('default')
+        variable = StringVar(self)
+        variable.set('Beginstation')
 
-    variable = StringVar(option1)
-    variable.set('Beginstation')
-    om1 = OptionMenu(option1, variable, *station_lijst, command=option_changed)
+        om1 = OptionMenu(self, variable, *station_lijst, command=option_changed)
 
-    B = tk.Button(window, text ="Volgende", bd = 5, width = 50, height = 4, bg = 'yellow', activebackground = 'yellow', command = window3, state = 'disabled')
-    w = Label(window, text="Kies uw beginstation", font=('Arial', 20), bg= 'yellow')
+        B = tk.Button(self, text ="Volgende", bd = 5, width = 50, height = 4, command = lambda: controller.show_frame(eindstationFrame), state = 'disabled')
+        w = Label(self, text="Kies uw beginstation", font=('Arial', 20))
 
-    w.pack()
-    om1.place(x = 100, y = 100)
-    om1.pack()
-    B.place(x = 130, y = 300)
-    B.pack()
+        w.pack()
+        om1.place(x = 100, y = 100)
+        om1.pack()
+        B.place(x = 130, y = 300)
+        B.pack()
 
 
-def window3():
-    window = tk.Tk()
-    window.geometry('400x200')
-    window.title("Beginstation")
-    window.configure(background='yellow')
+class eindstationFrame(tk.Frame):
 
-    option1 = window
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        def option_changed(a):
+            global eindstation,beginstation
+            eindstation = variable.get()
+            print(a,beginstation)
+            if a != beginstation:
+                B.config(state="normal")
+            else:
+                B.config(state="disabled")
 
-    def option_changed(a):
-        if a != 'Beginstation':
-            B.config(state='normal')
-        else:
-            B.config(state='disabled')
+        def save_and_show_voltooid():
+            save_reisgegevens(beginstation,eindstation)
+            controller.show_frame(voltooidFrame)
+        def save_reisgegevens(beginstation,eindstation):
+            global ovnummer
+            beginstationID = database.fetchOne(database.query("SELECT stationID FROM stations WHERE naam=\'%s\'" % beginstation))
+            eindstationID = database.fetchOne(database.query("SELECT stationID FROM stations WHERE naam=\'%s\'" % eindstation))
+            gebruikerID = database.fetchOne(database.query("SELECT gebruikerID FROM gebruikers WHERE ovnummer=%d" % ovnummer))
+            print(beginstation,eindstation,beginstationID,eindstationID,gebruikerID)
+            database.query("INSERT INTO reisgegevens(gebruikerID,beginstationID,eindstationID) VALUES ({0},{1},{2})".format(gebruikerID[0], beginstationID[0], eindstationID[0]))
+            database.save()
+        variable = StringVar(self)
+        variable.set('Eindstation')
+        station_lijst = genereer_stationlijst()
+        om1 = OptionMenu(self, variable, *station_lijst, command=option_changed)
+        B = tk.Button(self, text ="Volgende", bd = 5, width = 50, height = 4, command = save_and_show_voltooid, state = 'disabled')
+        w = Label(self, text="Kies uw eindstation", font=('Arial', 20))
 
-    variable1 = StringVar()
-    variable1.set('default')
-
-    variable = StringVar(option1)
-    variable.set('Eindstation')
-    om1 = OptionMenu(option1, variable, *station_lijst, command=option_changed)
-    B = tk.Button(window, text ="Volgende", bd = 5, width = 50, height = 4, bg = 'yellow', activebackground = 'yellow', command = window4, state = 'disabled')
-    w = Label(window, text="Kies uw eindstation", font=('Arial', 20), bg= 'yellow')
-
-    w.pack()
-    om1.place(x = 100, y = 100)
-    om1.pack()
-    B.place(x = 130, y = 300)
-    B.pack()
-
-def window4():
-
-    window = tk.Tk()
-    window.geometry('600x400')
-    window.title('Voltooid')
-    window.configure(background='yellow')
-
-
-    w = Label(window, text="Uw opdracht is voltooid!", font=('Arial', 20), bg= 'yellow')
-    w.pack()
-    w.place(x=170, y=150)
+        w.pack()
+        om1.place(x = 100, y = 100)
+        om1.pack()
+        B.place(x = 130, y = 300)
+        B.pack()
 
 
-# genereer eenmalig de gegevens die in de database staan zodat er makkelijk meer gewerkt kan worden
-station_lijst = genereer_stationlijst()
-ovnummer_lijst = genereer_ovnummerlijst()
+class voltooidFrame(tk.Frame):
 
-window = tk.Tk()
-window.geometry('600x400')
-window.title('Ns overzicht')
-window.configure(background='yellow')
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-T = Label(window, text="Welkom bij NS reis-database", font=('Arial', 20), bg= 'yellow')
-B = tk.Button(window, text ="Volgende", bd = 5, width = 50, height = 4, bg = 'yellow', activebackground = 'yellow', command = window2, state = 'disabled')
+        w = Label(self, text="Uw reisgegevens is opgeslagen!", font=('Arial', 20))
+        w.pack()
+        w.place(x=170, y=150)
 
-sv = tk.StringVar()
-sv.trace("w", lambda name, index, mode, sv=sv: callback(sv))
+        #qr code genereren
+        def generate_and_show_qr_code():
+            img = qrcode.make(database.fetchOne(database.query("SELECT reisID FROM reisgegevens ORDER BY reisID desc"))[0])
+            img.show()
 
-E1 = tk.Label(window, text="OV-nummer", bg = 'yellow')
-E2 = tk.Entry(window, textvariable=sv)
+        B = tk.Button(self, text ="Bekijk qr code", bd = 5, width = 50, height = 4, command = generate_and_show_qr_code)
+        B.place(x = 130, y = 300)
+        B.pack()
 
-def callback(sv):
-    try:
-        if int(E2.get()) in ovnummer_lijst:
-            print('Correct')
-            if E2.get():
-                B.config(state = 'normal')
-        else:
-            B.config(state = 'disabled')
-    except:
-        print('Retry')
-
-T.pack()
-E1.pack()
-E2.pack()
-B.pack()
-T.place(x = 140)
-E1.place(x = 130, y = 200)
-E2.place(x = 210, y = 200)
-B.place(x = 130, y = 300)
-window.mainloop()
+        # w = Label(self, image=img, font=('Arial', 20))
+        # w.pack()
+        # w.place(x=170, y=150)
 
 
-#qr code genereren
-img = qrcode.make("123")
+app = reisDatabase()
+app.geometry('600x400')
+app.title('Ns overzicht')
+app.mainloop()
  
